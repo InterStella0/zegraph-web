8a6910423420eee2c5a061b25add13bbe9453ff8
InterStella0 <70153286+InterStella0@users.noreply.github.com>
2026-06-13 14:40:55 +0800

    font weight

--- front-gfl/components/players/PlayerAliasesButton.tsx
+++ front-gfl/components/players/PlayerAliasesButton.tsx
@@ -26,7 +26,7 @@ export default function PlayerAliasesButton({ aliases }) {
                     <div className="divide-y divide-border">
                         {aliases.map((alias, i) => (
                             <div key={i} className="px-4 py-2">
-                                <div className="text-sm font-normal">
+                                <div className="text-sm font-semibold">
                                     {alias.name}
                                 </div>
                                 <div className="text-xs text-muted-foreground">
